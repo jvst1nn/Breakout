94db2c0cea75964adc17da60465d442774f29e81
jmarquez <jmarquez@student-bmhs-la.org>
2025-03-28 08:54:04 -0700

    commit

--- core/src/main/java/org/bmhsla/breakout/GameHandler.java
+++ core/src/main/java/org/bmhsla/breakout/GameHandler.java
@@ -1,5 +1,6 @@
 package org.bmhsla.breakout;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -7,6 +8,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.bmhsla.breakout.factories.EntityFactory;
+import org.bmhsla.breakout.systems.InputSystem;
+import org.bmhsla.breakout.systems.RenderSystem;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GameHandler extends ApplicationAdapter {
@@ -15,6 +19,12 @@ public class GameHandler extends ApplicationAdapter {
     @Override
     public void create() {
         engine = new Engine();
+
+        engine.addSystem(new RenderSystem());
+        engine.addSystem(new InputSystem());
+
+        EntityFactory entityFactory = new EntityFactory();
+        engine.addEntity(entityFactory.createPaddleEntity());
     }
 
     @Override
--- core/src/main/java/org/bmhsla/breakout/components/RenderComponent.java
+++ core/src/main/java/org/bmhsla/breakout/components/RenderComponent.java
@@ -1,16 +1,20 @@
 package org.bmhsla.breakout.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class RenderComponent implements Component {
+
     public enum ShapeKind {
         RECTANGLE,
         CIRCLE,
-        TRIANGLE,
+        TRIANGLE
     }
+
     public ShapeKind shape = ShapeKind.RECTANGLE;
     public ShapeRenderer.ShapeType shapeType = ShapeRenderer.ShapeType.Filled;
     public Color color = Color.WHITE;
+
 
 }
