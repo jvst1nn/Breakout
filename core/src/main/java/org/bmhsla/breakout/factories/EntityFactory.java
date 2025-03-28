58c858aa417dcbc68f157def39ea3a30719a6a0e
jmarquez <jmarquez@student-bmhs-la.org>
2025-03-28 09:30:05 -0700

    implement collision system

--- core/src/main/java/org/bmhsla/breakout/factories/EntityFactory.java
+++ core/src/main/java/org/bmhsla/breakout/factories/EntityFactory.java
@@ -1,6 +1,8 @@
 package org.bmhsla.breakout.factories;
 
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import org.bmhsla.breakout.components.*;
 
 public class EntityFactory {
@@ -13,7 +15,7 @@ public class EntityFactory {
         paddle.add(typeTag);
 
         PositionComponent positionComponent = new PositionComponent();
-        positionComponent.position.x = 10;
+        positionComponent.position.x = MathUtils.random(0, Gdx.graphics.getWidth());
         positionComponent.position.y = 10;
         paddle.add(positionComponent);
 
@@ -23,12 +25,45 @@ public class EntityFactory {
         paddle.add(sizeComponent);
 
         SpeedComponent speedComponent = new SpeedComponent();
-        speedComponent.speed = 200;
+        speedComponent.speed = 500;
         paddle.add(speedComponent);
 
         paddle.add(new RenderComponent());
 
         return paddle;
+    }
+
+    public Entity createBallEntity() {
+        Entity ball = new Entity();
+
+        TypeTagComponent typeTag = new TypeTagComponent();
+        typeTag.tag = "ball";
+        ball.add(typeTag);
+
+        PositionComponent positionComponent = new PositionComponent();
+        positionComponent.position.x = MathUtils.random(0, Gdx.graphics.getWidth());
+        positionComponent.position.y = MathUtils.random(Gdx.graphics.getHeight() / 2f, Gdx.graphics.getHeight());
+        ball.add(positionComponent);
+
+        SizeComponent sizeComponent = new SizeComponent();
+        sizeComponent.size.x = 10;
+        sizeComponent.size.y = 10;
+        ball.add(sizeComponent);
+
+        SpeedComponent speedComponent = new SpeedComponent();
+        speedComponent.speed = 500;
+        ball.add(speedComponent);
+
+        DirectionComponent directionComponent = new DirectionComponent();
+        directionComponent.direction.x = MathUtils.random(-1f, 1f);
+        directionComponent.direction.y = MathUtils.random(-1f, 1f);
+        ball.add(directionComponent);
+
+        RenderComponent renderComponent = new RenderComponent();
+        renderComponent.shape = RenderComponent.ShapeKind.CIRCLE;
+        ball.add(renderComponent);
+
+        return ball;
     }
 
 }
